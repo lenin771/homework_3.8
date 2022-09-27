7941fc336fb8998d614efc8bed6cf9d2db972f23
Your Name <you@example.com>
2022-09-27 15:35:08 +0300

    add task58

---
+++ task58/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Найти призведение двух матриц");
+
+int[,] matrix1 = new int[2, 2];
+int[,] matrix2 = new int[2, 2];
+int[,] sumMatrix = new int[2, 2];
+
+FillArray(matrix1);
+FillArray(matrix2);
+Console.WriteLine();
+Console.WriteLine("Матица А");
+PrintArray(matrix1);
+Console.WriteLine();
+Console.WriteLine("Матица В");
+PrintArray(matrix2);
+
+sumMatrix[0, 0] = matrix1[0, 0] * matrix2[0, 0] + matrix1[0, 1] * matrix2[1, 0];
+sumMatrix[1, 0] = matrix1[1, 0] * matrix2[0, 0] + matrix1[1, 1] * matrix2[1, 0];
+sumMatrix[0, 1] = matrix1[0, 0] * matrix2[0, 1] + matrix1[0, 1] * matrix2[1, 1];
+sumMatrix[1, 1] = matrix1[1, 0] * matrix2[0, 1] + matrix1[1, 1] * matrix2[1, 1];
+Console.WriteLine("Произведение матриц АхВ");
+PrintArray(sumMatrix);
